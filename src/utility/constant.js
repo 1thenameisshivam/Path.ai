35607f7a00bdbc688048e3e89d4d2cc68bbc333e
1thenameisshivam <1thenameisshivam@gmail.com>
2024-09-09 10:45:49 +0530

    firebase configured

--- src/utility/constant.js
+++ src/utility/constant.js
@@ -16,3 +16,12 @@ export const navData = [
     route: "/about",
   },
 ];
+
+export const {
+  VITE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_STORAGE_BUCCKET,
+  VITE_MESSAGE_SENDER_ID,
+  VITE_APP_ID,
+} = import.meta.env;
---
+++ src/utility/firebase.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  VITE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_STORAGE_BUCCKET,
+  VITE_MESSAGE_SENDER_ID,
+  VITE_APP_ID,
+} from "./constant";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCCKET,
+  messagingSenderId: VITE_MESSAGE_SENDER_ID,
+  appId: VITE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
